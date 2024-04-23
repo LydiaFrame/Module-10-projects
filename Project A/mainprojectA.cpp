3c8eb72208f154537c721e17dda91127c873c32a
LydiaFrame <156206633+LydiaFrame@users.noreply.github.com>
2024-04-23 21:18:34 +0000

    Finall with comments

--- Project A/mainprojectA.cpp
+++ Project A/mainprojectA.cpp
@@ -1,3 +1,15 @@
+/*
+    Program Name: Module 10 Project A 
+    Author: Lydia Frame 
+    Email: lydia.r.frame@gmail.com
+    Date: April 23, 2024
+    
+    Description:
+    This program demonstrates a simple example of a C++ program.
+    It includes a SecretType class with getter and setter methods, as well as a 
+    main function that creates an instance of the class and calls its methods.
+*/
+
 #include <iostream> 
 #include <string> 
 
@@ -68,16 +80,19 @@ class SecretType
 
 int main() 
 {
-
+    // Creat object num1
     SecretType num1;
 
+    // Call print function with default values 
     num1.print();
 
+    // Set new values
     num1.setName("Danberry"); 
     num1.setAge(23); 
     num1.setWeight(140);
     num1.setHeight(5.9); 
 
+    // Retrieve and output new values 
     cout << "Persons name is " << num1.getName() << ": They are " << num1.getAge() << " years old." << endl;
     cout << "They weigh: " << num1.getWeight() << "lb and is " << num1.getHeight() << " inches tall." << endl;  
 
