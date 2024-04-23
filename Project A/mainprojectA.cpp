e38a1fb1c6ef86e1556fda204445cbd201efbfef
LydiaFrame <156206633+LydiaFrame@users.noreply.github.com>
2024-04-23 20:48:15 +0000

    Program runs and produces correct output

--- Project A/mainprojectA.cpp
+++ Project A/mainprojectA.cpp
@@ -12,7 +12,7 @@ class SecretType
         double height; 
     public: 
         //constructor with defult parameters 
-        SecretType(string personName = "", int personAge = 0, int personWeight = 0, double personHeight = 0)
+        SecretType(string personName = "", int personAge = 0, int personWeight = 0, double personHeight = 0.0)
         {
             name = personName;
             age = personAge;
@@ -21,7 +21,7 @@ class SecretType
         }
 
         // Setter functions 
-        void setName(string &newName)
+        void setName(const string &newName)
         {
             name = newName;
         }
@@ -37,11 +37,49 @@ class SecretType
         {
             height = newHeight;
         }
+
+        // Getter functions 
+        string getName() const
+        {
+            return name;
+        }
+        int getAge() const
+        {
+            return age;
+        }
+        int getWeight() const
+        {
+            return weight;
+        }
+        double getHeight() const
+        {
+            return height;
+        }
+
+        // Print function
+        void print() 
+        {
+            cout << "Name: " << name << " "; 
+            cout << "Age: " << age << " "; 
+            cout << "Weight: " << weight << " ";
+            cout << "Height: " << height << endl; 
+        }
 };
 
 int main() 
 {
 
+    SecretType num1;
+
+    num1.print();
+
+    num1.setName("Danberry"); 
+    num1.setAge(23); 
+    num1.setWeight(140);
+    num1.setHeight(5.9); 
+
+    cout << "Persons name is " << num1.getName() << ": They are " << num1.getAge() << " years old." << endl;
+    cout << "They weigh: " << num1.getWeight() << "lb and is " << num1.getHeight() << " inches tall." << endl;  
 
 
     return 0; 
